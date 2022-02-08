5ce3f2e11d512f320fba6fe82426d36b091a541e
sheepfly <developer@noreply.email.com>
2022-02-08 23:12:33 +0800

    同步后台类型定义

--- src/models/global.ts
+++ src/models/global.ts
@@ -15,6 +15,50 @@ export interface GlobalModelState {
   notices: NoticeItem[];
 }
 
+/**
+ * 通用返回对象
+ */
+export interface DataObject {
+  /**
+   * 状态码。
+   */
+  statusCode: number;
+  /**
+   * 数据。
+   */
+  data: any;
+  /**
+   * 提示信息。
+   */
+  message: string;
+}
+
+/**
+ * 通用分页对象。
+ */
+export interface PageObject {
+  /**
+   * 当前页。
+   */
+  currentPage: number;
+  /**
+   * 页面容量。
+   */
+  pageSize: number;
+  /**
+   * 总页数。
+   */
+  totalPage: number;
+  /**
+   * 总条数。
+   */
+  totalRecords: number;
+  /**
+   * 数据列表。
+   */
+  dataList: any[],
+}
+
 export interface GlobalModelType {
   namespace: 'global';
   state: GlobalModelState;
