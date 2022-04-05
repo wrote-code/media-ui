c2b5a6e8ec13e1835da61992f79e99e5ff867f1f
sheepfly <developer@noreply.email.com>
2022-04-05 20:46:49 +0800

    增减添加站点界面

--- src/pages/Site/NewSiteModal.tsx
+++ src/pages/Site/NewSiteModal.tsx
@@ -1,7 +1,60 @@
+import { Form, Input, Modal } from 'antd';
 import React from 'react';
 
-const NewSiteModal: React.FC<> = (props) => {
-  return <div>hello.</div>;
+interface PropsType {
+  visible: boolean;
+  handleCancel: any;
+}
+
+
+const NewSiteModal: React.FC<any> = (props: PropsType) => {
+  const { handleCancel } = props;
+  const [form] = Form.useForm();
+
+  const handleOk = () => {
+    form.validateFields().then((values: any, errors: any) => {
+      if (errors) {
+        return false;
+      } else {
+        handleCancel(false);
+      }
+    });
+  };
+
+  return (
+    <Modal
+      okText="提交"
+      visible={props.visible}
+      title="新增站点"
+      onCancel={handleCancel}
+      onOk={handleOk}
+    >
+      <Form form={form}>
+        <Form.Item
+          label="站点名称"
+          name="siteName"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input placeholder="请输入站点名" />
+        </Form.Item>
+        <Form.Item
+          name="url"
+          label="网址"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input placeholder="请输入网址" />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
 };
 
 export default NewSiteModal;
--- src/pages/Site/index.tsx
+++ src/pages/Site/index.tsx
@@ -3,9 +3,10 @@ import type { SiteVo } from '@/models/types';
 import { fetchSiteVoListPro } from '@/services/site';
 import type { ProColumns } from '@ant-design/pro-table';
 import ProTable from '@ant-design/pro-table';
-import { Button } from 'antd';
+import Button from 'antd/es/button';
 import React, { useState } from 'react';
 import { connect } from 'umi';
+import NewSiteModal from './NewSiteModal';
 
 const columns: ProColumns<SiteVo>[] = [
   {
@@ -35,12 +36,30 @@ const columns: ProColumns<SiteVo>[] = [
 ];
 
 const Site: React.FC<SiteStateType> = (props) => {
+  const [newSiteModalVisible, setNewSiteModalVisible] = useState<boolean>(false);
+
+  const handleCancel = () => {
+    setNewSiteModalVisible(false);
+  };
+
   return (
     <div>
       <ProTable<SiteVo>
         columns={columns}
         request={async (params, sorter, filter) => fetchSiteVoListPro({ params, sorter, filter })}
+        toolBarRender={() => (
+          <Button
+            onClick={() => {
+              setNewSiteModalVisible(true);
+            }}
+          >
+            新建
+          </Button>
+        )}
       />
+      {newSiteModalVisible && (
+        <NewSiteModal handleCancel={handleCancel} visible={newSiteModalVisible} />
+      )}
     </div>
   );
 };
