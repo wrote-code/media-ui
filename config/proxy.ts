246efcab032c0777bf06c9b66cc117692bc96d30
wrote-code <developer@noreply.email.com>
2023-03-19 05:16:56 +0800

    修改代理端口为9000

--- config/proxy.ts
+++ config/proxy.ts
@@ -8,7 +8,7 @@
 export default {
   dev: {
     '/api': {
-      target: 'http://localhost:8080',
+      target: 'http://localhost:9000',
       changeOrigin: true,
       pathRewrite: { '^/api': '' },
     },
