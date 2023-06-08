c1922b5c0c0544f90fdc0319be6571f4ade9896d
wrote-code <developer@noreply.email.com>
2023-06-08 22:47:33 +0800

    从后台获取版本号

--- src/pages/ReleaseNote.tsx
+++ src/pages/ReleaseNote.tsx
@@ -1,17 +1,52 @@
+import request from '@/utils/request';
 import { Typography } from 'antd';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const { Title, Paragraph, Link } = Typography;
 
 interface PropsType {}
 const ReleaseNote: React.FC<PropsType> = () => {
+  const [version, setVersion] = useState();
+  const [backVersion, setBackVersion] = useState();
+  const [frontVersion, setFrontVersion] = useState();
+
+  useEffect(() => {
+    request('http://localhost:9000/version/main', {
+      method: 'GET',
+    })
+      .then((v) => setVersion(v))
+      .catch((e) => {
+        console.error(e);
+        setVersion(undefined);
+      });
+
+    request('http://localhost:9000/version/backVersion', {
+      method: 'GET',
+    })
+      .then((v) => setBackVersion(v))
+      .catch((e) => {
+        console.error(e);
+        setBackVersion(undefined);
+      });
+
+    request('http://localhost:9000/version/frontVersion', {
+      method: 'GET',
+    })
+      .then((v) => setFrontVersion(v))
+      .catch((e) => {
+        console.error(e);
+        setFrontVersion(undefined);
+      });
+  }, []);
+
   return (
     <Typography>
       <Title level={3}>版本信息</Title>
       <Paragraph>
         <ul>
-          <li>前台版本：v0.2.0-alpha</li>
-          <li>后台版本：v0.2.1-alpha</li>
+          <li>发行版本：{version}</li>
+          <li>前台版本：{frontVersion}</li>
+          <li>后台版本：{backVersion}</li>
         </ul>
       </Paragraph>
       <Title level={3}>发行说明</Title>
