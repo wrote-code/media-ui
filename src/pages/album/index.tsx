82142e2e0079e5425b71ad7d9a345ecb33d2265a
wrote-code <developer@noreply.email.com>
2024-02-20 20:39:40 +0800

    增加资源和专辑封面上传入口

--- src/pages/album/index.tsx
+++ src/pages/album/index.tsx
@@ -1,8 +1,9 @@
+import ImageUpload from '@/components/Common/upload/ImageUpload';
 import { queryAlbumList } from '@/services/album';
 import type { AlbumVo } from '@/types/entity';
 import type { ProColumns } from '@ant-design/pro-table';
 import ProTable, { ActionType } from '@ant-design/pro-table';
-import { Button, Input, Popover, message } from 'antd';
+import { Button, Input, Modal, Popover, message } from 'antd';
 import React, { useRef, useState } from 'react';
 import type { AlbumModelState } from 'umi';
 import { connect, useDispatch } from 'umi';
@@ -17,6 +18,7 @@ const Album: React.FC<PropsType> = () => {
   const [albumId, setAlbumId] = useState('');
   const [currentName, setCurrentName] = useState('');
   const [showDetail, setShowDetail] = useState(false);
+  const [showPreview, setShowPreview] = useState(false);
 
   const columns: ProColumns<AlbumVo>[] = [
     {
@@ -35,16 +37,28 @@ const Album: React.FC<PropsType> = () => {
       hideInForm: true,
       width: 50,
       render: (_, data: AlbumVo) => (
-        <Button
-          size="small"
-          onClick={() => {
-            setAlbumId(data.id);
-            setCurrentName(data.name);
-            setShowDetail(true);
-          }}
-        >
-          详情
-        </Button>
+        <Button.Group>
+          <Button
+            size="small"
+            onClick={() => {
+              setAlbumId(data.id);
+              setCurrentName(data.name);
+              setShowPreview(true);
+            }}
+          >
+            预览
+          </Button>
+          <Button
+            size="small"
+            onClick={() => {
+              setAlbumId(data.id);
+              setCurrentName(data.name);
+              setShowDetail(true);
+            }}
+          >
+            详情
+          </Button>
+        </Button.Group>
       ),
     },
   ];
@@ -112,6 +126,16 @@ const Album: React.FC<PropsType> = () => {
           onCancel={() => setShowDetail(false)}
           visible={showDetail}
         />
+      )}
+      {showPreview && (
+        <Modal
+          visible={showPreview}
+          title={`专辑【${currentName}】预览`}
+          onCancel={() => setShowPreview(false)}
+          onOk={() => setShowPreview(false)}
+        >
+          <ImageUpload businessCode={albumId} businessType={4} />
+        </Modal>
       )}
     </>
   );
